a1aa33418aa85cbc492cf793fe993b639e2e50f0
Karlo Reuben Sadural <saduralk@purdue.edu>
2023-08-23 14:43:13 -0400

    Updated the text.

--- ConsoleApplication1/ConsoleApplication1.cpp
+++ ConsoleApplication1/ConsoleApplication1.cpp
@@ -5,7 +5,7 @@
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::cout << "The text has been changed to be different now.\nNow there is more text!";
 }
 
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
